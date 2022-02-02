84a668b3cf55d46bd373f1485a1a31ea046bced4
amykoh29 <amykoh29@gmail.com>
2022-02-01 21:17:57 -0800

    added lib and test method

--- Demo.java
+++ Demo.java
@@ -1,1 +1,10 @@
-
+public class Demo{
+    public static int countInstanceOf(String word, String find){
+        int index =0, counter=0;
+        while(index<word.length() && word.indexOf(find, index)!=-1){
+            counter++;
+            index = word.indexOf(find, index) + word.length();
+        }
+        return counter;
+    }
+}
--- DemoTest.java
+++ DemoTest.java
@@ -1,1 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
 
+public class DemoTest{
+    @Test
+    public void countInstanceOfTest(){
+        assertEquals(3, Demo.countInstanceOf("Happy Lunar New Year", "e")); //false, should return 2
+    }
+}
